2cad36d8b0dd7938ad18978986348832abba18b4
Ilce5 <ilcenaumovskics@gmail.com>
2022-05-25 18:30:13 +0200

    Vnes na sodrzina i adresa vo vektor

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+	string sodrzina;
+	string adresa;
+	vector<int> ascii;
+	int golemina = 0;
+	int broj = 0;
+
+	cout<<"Vnesete sodrzina: ";
+	cin>>sodrzina;
+	cout<<"Vnesete validna adresa: ";
+	cin>>adresa;
+	cout<<"\n------------------------------------------------\n"<<sodrzina<<" "<<adresa<<"\n------------------------------------------------\n"<<endl;
+
+	golemina = sodrzina.length(); //Vnesuvanje na sodrzinata kako ASCII kod vo vektor
+	for(int i = 0; i < golemina; i++){
+		broj = int(sodrzina.at(i));
+		ascii.push_back(broj);
+	}
+
+	golemina = adresa.length(); //Vnesuvanje na adresata kako ASCII kod vo vektor
+	for(int i = 0; i < golemina; i++){
+		broj = int(adresa.at(i));
+		ascii.push_back(broj);
+	}   
+}
+    
